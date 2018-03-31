0796b3aa6045795ace7a480d61378fba1951748d
lstockburg <lstockburg@gmail.com>
2018-03-30 19:51:12 -0700

    add variables, comment text seperating user/business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 //Business logic
   function
+
+// User Logic
+$(document).ready(function() {
+  $("#user-input-form").submit(function(event) {
+    event.preventDefault();
+    var userInput=
+    $("#user-input").val();
+
+    var userNum =
+    userInput.match(/ /)
+
+
+  });
