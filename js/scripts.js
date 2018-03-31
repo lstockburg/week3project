58e130bd86d7486bab133f85b1c9326361a03ce7
lstockburg <lstockburg@gmail.com>
2018-03-30 20:58:31 -0700

    add some back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //Business logic
-  function
-
+function eatNum(array) {
+  var i = ('userInput')
+  return(i);
+}
 // User Logic
 $(document).ready(function() {
   $("#user-input-form").submit(function(event) {
@@ -8,8 +10,12 @@ $(document).ready(function() {
     var userInput=
     $("#user-input").val();
 
-    var userNum =
-    userInput.match(/ /)
+    var numArray =
+    userInput.split("").push();
+    $("#output").empty().text();
 
 
   });
+  console.log()
+  debugger
+});
