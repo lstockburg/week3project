65e58289a9701fe6971ab4d468c7cdfe4e0d9359
lstockburg <lstockburg@gmail.com>
2018-04-02 21:54:27 -0700

    change code to try something else entirely. nothing is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,12 @@
 //Business logic
-function convertNum(numbersArray) {
-  var numbersArray = userInput.split("");
-
-    if (numbersArray.match[/0/g]) {
-      return ('boop');
-    }
-    if (numbersArray.match[/1/g]) {
-      return ('beep');
-    }
-};
-
-
-
-
-
 
 // User Logic
 $(document).ready(function() {
   $("#user-input-form").submit(function(event) {
     event.preventDefault();
-    //Grab input from user form, set to variable
     var userInput=
     $("#user-input").val();
 
-    $("#output").empty().append(convertNum());
-  });
 
-console.log(numbersArray)
+  });
 });
