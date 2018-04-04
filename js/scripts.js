c4e1ade755194ff3bb06f26e08c6c8ad1ced0eb6
lstockburg <lstockburg@gmail.com>
2018-04-04 11:13:54 -0700

    adjust variables in backend for functional code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 //Business logic
-
+function eatNum(number) {
+  var numArray = [];
+  for (var n = 0; n <= number; n++) {
+    var numArray = n.toString().split('');
+    if (n % 3 === 0) {
+      numArray.push("I'm sorry Dave");
+    } else if(numArray.includes("1")) {
+      numArray.push(" Boop");
+    } else if(numArray.includes("0")) {
+      numArray.push(" Beep");
+    } else {
+      numArray.push(" " + n);
+    }
+  }
+  alert(numArray)
+  return numArray;
+};
 // User Logic
 $(document).ready(function() {
   $("#user-input-form").submit(function(event) {
     event.preventDefault();
-    var userInput=
+    var userInput =
     $("#user-input").val();
 
+    $("#output h2").text(eatNum(userInput));
+    $("#output").show();
 
+    debugger
   });
 });
